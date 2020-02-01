373c8088752ed7492c3d0ca1ea2f81e1a9878595
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-01 20:17:41 +0300

    a typo fix

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/goinaction/code/chapter2/sample/matchers "
+	_ "github.com/goinaction/code/chapter2/sample/matchers"
 	"github.com/goinaction/code/chapter2/sample/search"
 )
 
