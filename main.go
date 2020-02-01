32a3c7d3d098645cc12792c6e7a9f9d6f172a9f1
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-01 20:13:54 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"log"	
+"os"
+
+_ "github.com/goinaction/code/chapter2/sample/matchers"
+"github.com/goinaction/code/chapter2/sample/search"
+)
+
+func init() {
+log.SetOutput(os.Stdout)
+}
+
+func main() {
+	fmt.Println("Hello, playground")	
+search.Run("president")
+}
